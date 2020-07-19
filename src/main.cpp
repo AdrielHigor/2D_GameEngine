73623a525de7c6fbd4daf987e3cea8be362eb715
AdrielHigor <adrielhigor@gmail.com>
2020-07-19 08:44:25 -0300

    ADD: assets and dlls

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+// to compile, run the following code
+// g++ -o Game main.cpp Game.cpp -lSDL2 -lSDL2main 
+
+Game *game = nullptr;
+
+int main(int argc, char * argv[]){
+
+    game = new Game();
+    
+    game->init("Teste", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
+
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+
+    game->clean();
+    
+    return 0;
+}
